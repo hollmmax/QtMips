d392d98e997d9fcf79432505f285a9e1d0d84edb
Max Hollmann <hollmannm1@gmail.com>
2020-09-30 02:49:47 +0900

    build.sh now uses a build directory and make utilises all threads

--- build.sh
+++ build.sh
@@ -2,14 +2,19 @@
 set -e
 
 ROOT="$(dirname "$(readlink -f "$0")")"
+BUILDDIR=build
 
-mkdir -p build
-cd build
+mkdir -p "$BUILDDIR"
+cd "$BUILDDIR"
+
 
 # Compile
-qtchooser -run-tool=qmake -qt=5 -recursive "$ROOT" "QMAKE_RPATHDIR += ../qtmips_machine ../qtmips_osemu ../qtmips_asm"
-make sub-qtmips_cli sub-qtmips_gui # Note: we are building these to to not build tests
+# qtchooser -run-tool=qmake -qt=5 -recursive "$ROOT" "QMAKE_RPATHDIR += ../qtmips_machine ../qtmips_osemu ../qtmips_asm"
+qmake ..
+# make -j8 sub-qtmips_cli sub-qtmips_gui # Note: we are building these to to not build tests
+make -j$(nproc)
 
+cd ..
 # Link executables to more suitable place
-ln -fs qtmips_cli/qtmips_cli cli
-ln -fs qtmips_gui/qtmips_gui gui
+ln -fs "$BUILDDIR/qtmips_cli/qtmips_cli" cli
+ln -fs "$BUILDDIR/qtmips_gui/qtmips_gui" gui
